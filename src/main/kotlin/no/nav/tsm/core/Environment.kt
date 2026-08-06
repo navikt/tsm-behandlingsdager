c21da32dcfe2fd298af2cf8ff641a9ef8b6d901c
Karl J. Overå <k@rl.run>
2026-08-06 14:16:45 +0200

    refactor: go all in on tsm-ktor features

--- src/main/kotlin/no/nav/tsm/core/Environment.kt
+++ src/main/kotlin/no/nav/tsm/core/Environment.kt
@@ -1,22 +1,15 @@
 package no.nav.tsm.core
 
-import io.ktor.server.application.*
 import io.ktor.server.config.*
-import io.ktor.server.plugins.di.*
-import java.util.*
 import kotlin.time.Duration
+import no.nav.tsm.ktor.nais.RuntimeCluster
+import no.nav.tsm.ktor.nais.getRuntimeCluster
 
-enum class RuntimeEnvironments(val nais: String) {
-    LOCAL("local"),
-    DEV("dev-gcp"),
-    PROD("prod-gcp"),
-}
-
-class Runtime(val env: RuntimeEnvironments, val name: String)
+class Runtime(val env: RuntimeCluster, val name: String)
 
 class Environment(
     val runtime: Runtime,
-    val kafka: KafkaConfig,
+    val sykmeldingerConsumer: SykmeldingerConsumer,
     val external: ExternalConfig,
     val behandlingsdagerIds: List<String>,
 )
@@ -25,47 +18,21 @@ data class ExternalConfig(val tsmPdlCache: TsmPdlConfig)
 
 data class TsmPdlConfig(val url: String)
 
-class KafkaConfig(val config: Properties, val pollInterval: Duration)
+class SykmeldingerConsumer(val pollInterval: Duration)
 
 fun initializeEnvironment(config: ApplicationConfig): Environment {
-    val kafkaProperties =
-        KafkaConfig(
-            config =
-                Properties().apply {
-                    config.config("kafka.config").toMap().forEach { this[it.key] = it.value }
-                },
-            pollInterval = config.property("kafka.sykmeldingConsumer.longPoll").getAs(),
-        )
     return Environment(
         runtime =
             Runtime(
-                env = config.inferRuntimeEnvironment(),
+                env = getRuntimeCluster(),
                 name = config.property("app.name").getString(),
             ),
-        kafka = kafkaProperties,
+        sykmeldingerConsumer =
+            SykmeldingerConsumer(pollInterval = config.property("sykmeldingConsumer.longPoll").getAs()),
         external = ExternalConfig(TsmPdlConfig(url = config.property("external.tsmPdlCache.url").getString())),
         behandlingsdagerIds =
             config.property("behandlingsdager.ids").getString().split(',').filter {
                 it.isNotEmpty()
             },
     )
-}
-
-fun Application.isLocal(): Boolean {
-    val env: Environment by dependencies
-
-    return env.runtime.env == RuntimeEnvironments.LOCAL
-}
-
-private fun ApplicationConfig.inferRuntimeEnvironment(): RuntimeEnvironments {
-    return when (val configEnv = this.property("app.runtime").getString()) {
-        "local" -> RuntimeEnvironments.LOCAL
-        "prod-gcp" -> RuntimeEnvironments.PROD
-        "dev-gcp" -> RuntimeEnvironments.DEV
-        else -> {
-            throw IllegalStateException(
-                "Unexpected 'app.runtime' configuration: ${configEnv}. Should be one of 'local', 'dev-gcp' or 'prod-gcp'"
-            )
-        }
-    }
 }
